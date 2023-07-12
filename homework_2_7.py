cb602fe0752526b21c580f5e9ed1f36c38b94d26
Nextoni <mariqn2602@abv.bg>
2023-07-12 11:46:05 +0300

    Update homework_2_7.py

--- homework_2_7.py
+++ homework_2_7.py
@@ -7,15 +7,17 @@ def count_substrings(haystack, needle):
         
     return result
 
-print(count_substrings("Python is an awesome language to program in!", "o"))
+print(count_substrings("babababa", "baba"))
+
+
 
 def count_substrings1(haystack, needle):
     result = 0
 
-    for itr in haystack:
-        if needle in itr:
+    for char in range(len(haystack)):
+        if haystack[char : char + len(needle)] == needle:
             result += 1
-    
+
     return result
 
-print(count_substrings1("Python is an awesome language to program in!", "o"))
+print(count_substrings1("babababa", "baba"))
